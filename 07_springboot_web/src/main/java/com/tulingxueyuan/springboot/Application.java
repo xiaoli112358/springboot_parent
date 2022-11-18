59d11c798e0bf927ef014d3e8d0301c6fb51fc2c
xiaoli112358 <1246468641@qq.com>
2022-11-18 22:54:44 +0800

    mybatisplus分页拦截器解释

--- 07_springboot_web/src/main/java/com/tulingxueyuan/springboot/Application.java
+++ 07_springboot_web/src/main/java/com/tulingxueyuan/springboot/Application.java
@@ -16,10 +16,23 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * 该方法其实就是一个拦截器，做分页的拦截
+     * @return
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 添加分页插件
+        PaginationInnerInterceptor pageInterceptor = new PaginationInnerInterceptor();
+        // 设置请求的页面大于最大页后操作，true调回到首页，false继续请求。默认false
+        pageInterceptor.setOverflow(false);
+        // 单页分页条数限制，默认无限制
+        pageInterceptor.setMaxLimit(500L);
+        // 设置数据库类型
+        pageInterceptor.setDbType(DbType.MYSQL);
+
+        interceptor.addInnerInterceptor(pageInterceptor);
         return interceptor;
     }
 }
